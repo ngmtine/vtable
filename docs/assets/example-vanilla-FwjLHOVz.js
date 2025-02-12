b4976466c8790453d369fda0170bae4ee883f58f
ngmtine <ngmfries@gmail.com>
2025-02-13 08:52:38 +0900

    rebuild

--- docs/assets/example-vanilla-FwjLHOVz.js
+++ docs/assets/example-vanilla-FwjLHOVz.js
@@ -1,4 +1,4 @@
-import"./index-CuX8Uz3M.js";import{g as r,o,a as c}from"./data-B4a1iIrO.js";const d=()=>`
+import"./index-CuX8Uz3M.js";import{g as r,o,a as c}from"./data-BPnHUmLj.js";const d=()=>`
 <tr class="header-row">
     <th class="cell">ID</th>
     <th class="cell">Name</th>
@@ -17,4 +17,4 @@ import"./index-CuX8Uz3M.js";import{g as r,o,a as c}from"./data-B4a1iIrO.js";cons
     <td class="cell" colspan="4" style="text-align: center;">Footer Content</td>
 </tr>
 `,g=t=>{const l=r(1e5);let e=window.innerHeight-100;const a=30,s=new o({recordList:l,containerHeight:e,rowHeight:a,uniqueKey:"id",headerRenderer:d,bodyRenderer:i,footerRenderer:h,extraItemCount:40});s.mount(t),window.addEventListener("resize",()=>{e=window.innerHeight-100,s.containerHeight=e,s.tableContainer.style.height=`${e}px`,s.renderRows()})},n=document.getElementById("root");n&&g(n);
-//# sourceMappingURL=example-vanilla-DP3VHdfQ.js.map
+//# sourceMappingURL=example-vanilla-FwjLHOVz.js.map
--- docs/assets/index-miCdZFtF.js
+++
@@ -1,2 +0,0 @@
-import"./index-CuX8Uz3M.js";import{c as l,j as e}from"./client-f9UO3wli.js";const r=()=>e.jsxs("div",{children:[e.jsx("h1",{children:"Vtable Examples"}),e.jsxs("nav",{children:[e.jsx("a",{href:"example-vanilla/index.html",children:"Example Vanilla"}),e.jsx("br",{}),e.jsx("a",{href:"example-react/index.html",children:"Example React"}),e.jsx("br",{}),e.jsx("a",{href:"example-vue/index.html",children:"Example Vue"})]})]});l.createRoot(document.getElementById("root")).render(e.jsx(r,{}));
-//# sourceMappingURL=index-miCdZFtF.js.map
